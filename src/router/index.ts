25ba3ffc3a89142f8bb98bd708b4306fafb66795
ssumthingood <ssumthingood@gmail.com>
2024-04-12 14:02:25 +0900

    router 설정 변경 -> default(NotFound) 리다이렉트 처리

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,7 +13,15 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
+    // lazy-loading -> 효율적인 페이지 로딩 속도 / 효율적 리소스 사용 가능
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    // NotFound -> vue3부터 '*' 이 아닌 '/:pathMatch(.*)*' 사용
+    redirect: '/',
+    // 아래처럼 바로 NotFound 경로를 매칭해도 됨
+    // component: NotFound
   },
 ];
 
